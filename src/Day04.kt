0b243058c7633df2ff5a70a29221a3eb3f19346b
vladislav-og <vladislav.ogorodnik@mail.ru>
2023-12-04 19:56:42 +0200

    Day 4 refactor

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,11 +1,13 @@
+import kotlin.math.pow
+
 fun main() {
 
-    fun getNumbers(text: String) = Regex("[0-9]+").findAll(text).map { it.value.toInt() }.toList()
+    fun getNumbers(text: String) = text.chunked(3).map { it.trim().toInt() }
 
     fun getWinningCount(myNumsDirty: String, winningNumsDirty: String): Int {
         val myNums = getNumbers(myNumsDirty)
         val winningNums = getNumbers(winningNumsDirty)
-        var matchedNums = 0;
+        var matchedNums = 0
         for (num in myNums) {
             if (winningNums.contains(num)) {
                 matchedNums++
@@ -14,17 +16,22 @@ fun main() {
         return matchedNums
     }
 
+    fun getMatchedPoints(game: String): Int {
+        val (_, myNumsDirty, winningNumsDirty) = game.split(": ", " | ")
+        val matchedNums = getWinningCount(myNumsDirty, winningNumsDirty)
+        return matchedNums
+    }
+
     fun part1(input: List<String>): Int {
         var score = 0
         for (game in input) {
-            val (myNumsDirty, winningNumsDirty) = game.split(":")[1].split("|")
-            val matchedNums = getWinningCount(myNumsDirty, winningNumsDirty)
-            score += Math.pow(2.0, matchedNums.toDouble() - 1.0).toInt()
+            val matchedNums = getMatchedPoints(game)
+            score += 2.0.pow(matchedNums.toDouble() - 1.0).toInt()
         }
         return score
     }
 
-    fun createScrathcardsMap(input: List<String>): MutableMap<Int, Int> {
+    fun createScratchCardsMap(input: List<String>): MutableMap<Int, Int> {
         val scratchcards = mutableMapOf<Int, Int>()
         for (i in input.indices) {
             scratchcards[i] = 1
@@ -33,10 +40,9 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val scratchcards = createScrathcardsMap(input)
+        val scratchcards = createScratchCardsMap(input)
         for ((gameNumber, game) in input.withIndex()) {
-            val (myNumsDirty, winningNumsDirty) = game.split(":")[1].split("|")
-            val matchedNums = getWinningCount(myNumsDirty, winningNumsDirty)
+            val matchedNums = getMatchedPoints(game)
 
             for (i in 1..matchedNums) {
                 scratchcards[gameNumber + i] = scratchcards[gameNumber + i]!! + scratchcards[gameNumber]!!
@@ -54,5 +60,7 @@ fun main() {
 
     val input = readInput("Day04")
     part1(input).println()
+    check(part1(input) == 23941)
     part2(input).println()
+    check(part2(input) == 5571760)
 }
